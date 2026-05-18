ac6418e4f826cd1b1311f3a1c96ecb3369f92695
steinsein <steinsfactory@gmail.com>
2026-05-19 00:40:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from src import questions, results, utils
 # =============================================================================
 
 st.set_page_config(
-    page_title="RDAP 종교 다양성 태도 프로파일 (퀵버전)",
+    page_title="종교 다양성 태도 프로파일qv",
     page_icon="🪞",
     layout="centered",
     initial_sidebar_state="collapsed",
